a9b2f2f09839497bc667f5a6a3aff09c9940f3ce
Michael Kainer <stuff@pushrax.com>
2025-12-24 10:09:45 +0100

    update.sh modified - 24/12/25 - 10:09:40

---
+++ docs/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+pushd ~/klipper
+
+make clean
+make menuconfig KCONFIG_CONFIG=/home/michael/printer_data/config/ebb36_v1_2.config
+make -j$(nproc) KCONFIG_CONFIG=/home/michael/printer_data/config/ebb36_v1_2.config
+
+popd
