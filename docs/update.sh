43f4a187da0716aff9fff9c616028ec971661bdc
Michael Kainer <stuff@pushrax.com>
2025-12-24 10:09:58 +0100

    update.sh modified - 24/12/25 - 10:09:54

--- docs/update.sh
+++ docs/update.sh
@@ -7,4 +7,7 @@ make clean
 make menuconfig KCONFIG_CONFIG=/home/michael/printer_data/config/ebb36_v1_2.config
 make -j$(nproc) KCONFIG_CONFIG=/home/michael/printer_data/config/ebb36_v1_2.config
 
+make clean
+make menuconfig KCONFIG_CONFIG=/home/michael/printer_data/config/esp32s3_ebbev1_2.config
+
 popd
