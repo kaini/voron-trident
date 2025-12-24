5fec06a850faed5e2fc0c29ea164f36fb189d22c
Michael Kainer <stuff@pushrax.com>
2025-12-24 10:14:22 +0100

    update.sh modified - 24/12/25 - 10:14:17

--- docs/update.sh
+++ docs/update.sh
@@ -8,6 +8,7 @@ pushd ~/klipper
 make clean
 make menuconfig KCONFIG_CONFIG=/home/michael/printer_data/config/ebb36_v1_2.config
 make -j$(nproc) KCONFIG_CONFIG=/home/michael/printer_data/config/ebb36_v1_2.config
+python3 ~/katapult/scripts/flashtool.py -i can0 -r -u yourtoolheaduuid
 
 make clean
 make menuconfig KCONFIG_CONFIG=/home/michael/printer_data/config/m8p.config
